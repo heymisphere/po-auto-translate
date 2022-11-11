40f186b5d7a487cdfa167d13f69fc75f014a718c
Hemish <hemish04082005@gmail.com>
2022-11-12 02:18:03 +0530

    Initial

---
+++ main.py
@@ -0,0 +1,26 @@
+from argparse import ArgumentParser
+from polib import pofile
+from pylingva import pylingva
+translator = pylingva()
+
+ap = ArgumentParser(description="Auto-translate PO files")
+ap.add_argument("--file", '-s', nargs=1, required=True, help="File", metavar='/path/to/file')
+ap.add_argument("--source", '-f', nargs=1, required=False, metavar='lang', help="Source language, default: en")
+ap.add_argument("--to", '-t', nargs=1, required=True, metavar='lang', help="Target language")
+
+args = ap.parse_args()
+
+src = args.source
+if src == None:
+    src = 'en'
+else:
+    src = src[0]
+
+des = args.to[0]
+file = pofile(args.file[0])
+
+for entry in file.untranslated_entries():
+    l = entry.msgstr = translator.translate(src, des, entry.msgid)
+    entry.msgstr = l
+
+file.save(args.file[0]+'-translated')
